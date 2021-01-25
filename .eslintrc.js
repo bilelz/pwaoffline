4699587437e96cc55aa523123a104dc722aee254
Bilelz <extern.bilel.zeghad@europcar.com>
2021-01-25 17:16:24 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+
+const prettierConfig = require('./.prettierrc');
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended','@vue/prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "prettier/prettier": ["error", { "singleQuote": true }]
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ src/components/FilesList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="files-list">
+    <ul>
+      <li v-for="(file, index) in list" :key="index">
+        <a :href="file.path">
+          {{ file.name }}
+        </a>
+      </li>
+    </ul>
+
+    <button @click="onClick">Click!</button> {{ count }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FilesList extends Vue {
+  // Initial data can be declared as instance properties
+  message = 'Hello!';
+
+  list = [
+    {
+      name: 'pdf',
+      path: '../assets/CROQUIS 3 D761.pdf',
+    },
+    {
+      name: 'fullstack',
+      path: '../assets/fullstack.jpg',
+    },
+  ];
+
+  count = 0;
+
+  onClick(): void {
+    this.count += 1;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+
+a {
+  color: #42b983;
+}
+</style>
